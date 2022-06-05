09aa2cd1f294a36e8c603a0f3bab562b0f7587ab
Matheus Foliatti <106719872+MatheusFoliatti@users.noreply.github.com>
2022-06-04 21:46:31 -0300

    Add files via upload

---
+++ React/contagemporclique/src/components/navbar/navbar.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./navbar.css";
+export default function navbar() {
+  return (
+    <nav className="navbar">
+      <span> Contagem de Números</span>
+    </nav>
+  );
+}
