962495e37c1f4f6fb3f3f1e0b13516187e6ce406
Matheus Foliatti <106719872+MatheusFoliatti@users.noreply.github.com>
2022-06-04 21:46:18 -0300

    Delete contagemporclique directory

--- contagemporclique/src/components/navbar/navbar.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import "./navbar.css";
-export default function navbar() {
-  return (
-    <nav className="navbar">
-      <span> Contagem de Números</span>
-    </nav>
-  );
-}
