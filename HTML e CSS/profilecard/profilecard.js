393cf64abe9faf7856a5d9c9de49253ce808a39b
Matheus Foliatti <106719872+MatheusFoliatti@users.noreply.github.com>
2022-06-17 05:07:59 -0300

    Add files via upload

---
+++ HTML e CSS/profilecard/profilecard.js
@@ -0,0 +1,10 @@
+const follow = document.querySelector(".actionBtn, .follow");
+const message = document.querySelector(".actionBtn, .message");
+
+follow.addEventListener('click', () => {
+    window.open("https://www.instagram.com/foliattimatheus/")
+})
+
+message.addEventListener('click', () => {
+    window.open("https://www.instagram.com/foliattimatheus/")
+})
